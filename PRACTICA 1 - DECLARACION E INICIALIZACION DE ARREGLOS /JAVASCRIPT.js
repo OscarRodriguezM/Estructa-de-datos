433c8b07e270637994ac774b1c18a778bb650116
OscarRodriguezM <112915527+OscarRodriguezM@users.noreply.github.com>
2025-09-09 13:57:25 -0600

    JAVASCRIPT.js

---
+++ PRACTICA 1 - DECLARACION E INICIALIZACION DE ARREGLOS /JAVASCRIPT.js
@@ -0,0 +1,14 @@
+// FORMA COMUN DE DECLARAR LOS ARREGLOS, LES ASIGNO UN TAMAÑO Y LISTO
+let ar1 = new Array(4);  
+let ar2 = new Array(4);  
+let ar3 = new Array(4);  
+
+// LES ASIGNO UN TAMAÑO Y VALORES YA INICIALIZADOS O LLENADO
+let ar4 = [1, 2, 3, 4];  
+let ar5 = ['a', 'b', 'c', 'd'];  
+let ar6 = [2.4, 3.0, 23.0, 6.0];  
+
+// ESTA FORMA NO LA ENTIENDO MUY BIEN, ARREGLO DINAMICO
+let arr = Array.from({length: 4}, () => 0);  
+
+console.log("PRACTICA NO.1 DECLARACION E INICIALIZACION DE ARREGLOS EN JAVASCRIPT");
