eac253ecb3fc25b059ed531ec78084047876f92d
OscarRodriguezM <112915527+OscarRodriguezM@users.noreply.github.com>
2025-09-09 12:54:46 -0700

    Add files via upload

---
+++ PRACTICA 1 - DECLARACION E INICIALIZACION DE ARREGLOS/JAVA.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        // FORMA COMUN DE DECLARAR LOS ARREGLOS, LES ASIGNO UN TAMAÑO Y LISTO
+        int[] ar1 = new int[4];
+        char[] ar2 = new char[4];
+        float[] ar3 = new float[4];
+
+        // LES ASIGNO UN TAMAÑO Y VALORES YA INICIALIZADOS O LLENADO
+        int[] ar4 = {1, 2, 3, 4};
+        char[] ar5 = {'a', 'b', 'c', 'd'};
+        float[] ar6 = {2.4f, 3.0f, 23.0f, 6.0f};
+
+        // ESTA FORMA NO LA ENTIENDO MUY BIEN, ARREGLO DINAMICO
+        int[] arr = new int[4];
+
+        System.out.println("PRACTICA NO.1 DECLARACION E INICIALIZACION DE ARREGLOS EN JAVA");
+    }
+}
+
